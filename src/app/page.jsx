e665bf6c54549d4ede8a255b6847150fbe5bf0f1
Waled Karim <waledkarim445@gmail.com>
2026-04-02 01:15:38 +0600

    environment_variable

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import ClientComponent from "@components/Client";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
           height={20}
           priority
         />
+        <ClientComponent />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="font-bengal max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             হ্যালো সবাই! কেমন আছেন?
@@ -51,7 +53,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
---
+++ src/components/Client.jsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ClientComponent() {
+  console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
+}
